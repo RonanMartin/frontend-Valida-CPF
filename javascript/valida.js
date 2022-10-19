d1f9683f9e0898562f71f7831e217abef2a6a1a9
Ronan Martin <ronanmartin86@outlook.com>
2022-10-19 19:36:51 -0300

    Incluso limitador para números no input, retirada do autocomplete, e aviso de CPF incompleto

--- javascript/valida.js
+++ javascript/valida.js
@@ -1,10 +1,17 @@
 
               // MODEL - SECTION
+
+              
+
                 function validaCPF (cpf){
                   var API_URL = "http://localhost:8080/valida-cpf?numero=" + cpf
                   
                   const input = document.getElementById('cpf');
 
+                  const cpfOk = document.createElement('div');
+                  cpfOk.style = 'margin-bottom: 25px; margin-top: 0px; color:rgb(19, 122, 67)'
+
+                  console.log(input)
 
               fetch(API_URL)
               .then(res => res.json())
@@ -14,9 +21,7 @@
                   document.getElementById('resp').innerHTML = '';
                                       
                   input.style = 'background-color: rgb(131, 247, 208);'
-
-                  const cpfOk = document.createElement('div');
-                  cpfOk.style = 'margin-bottom: 25px; margin-top: 0px; color:rgb(19, 122, 67)'
+                  console.log(input)                  
 
                   cpfOk.innerText = res.formatado + ' CPF válido!'
                   
@@ -26,9 +31,9 @@
                 } else {
                   document.getElementById('resp').innerHTML = '';
 
-                  input.style = 'background-color: rgb(255, 122, 122);'
+                  input.style = 'background-color: rgb(255, 195, 195);'
+                  console.log(input)
 
-                  const cpfOk = document.createElement('div');
                   cpfOk.style = 'margin-bottom: 25px; margin-top: 0px; color:rgb(222, 40, 40)'
 
                   cpfOk.innerText = res.formatado + ' CPF inválido!'                                      
@@ -42,12 +47,27 @@
               // CONTROLER - SECTION
 
                 
+
                 function getCPF() {
-                  const cpfbox = document.getElementById('cpf');
-                  const cpf = cpfbox.value;
-                  validaCPF(cpf)
-              }
-                
+                  const input = document.getElementById('cpf');
+                  const cpf = input.value;
+                  const larg = cpf.length
+                    if (larg < 11) {
+                      document.getElementById('resp').innerHTML = '';
+
+                      input.style = 'background-color: rgb(255, 195, 195);'
+                      const cpfInc = document.createElement('div');
+                      cpfInc.style = 'margin-bottom: 25px; margin-top: 0px; color:rgb(222, 40, 40)'
+
+                      cpfInc.innerText = 'CPF incompleto!'                                      
+
+                      const CPFincompleto = document.getElementById('resp');
+                      CPFincompleto.appendChild(cpfInc); 
+                    } else {
+                      validaCPF(cpf)
+                        }
+                }
+                      
               // FUNÇÃO BASEADA EM REQUISIÇÃO XHR 
               //function validaCPF (cpf){
                // var API_URL = "http://localhost:8080/valida-cpf?numero=" + cpf
